d14909207e5736d477df41e7df25aa95d6990665
Chí Vương Nguyễn <nguyenchivuong.73@gmail.com>
2026-06-16 22:32:40 +0700

    fix deploy

--- src/pages/settings/useAccountSettings.ts
+++ src/pages/settings/useAccountSettings.ts
@@ -109,6 +109,7 @@ export function useAccountSettings() {
         newPassword,
         setNewPassword,
         handleUpdateProfile,
-        handleUpdatePassword
+        handleUpdatePassword,
+        hasPassword
     }
 }
